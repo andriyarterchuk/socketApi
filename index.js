da7acd219b36a4428eb46060262b7556c7921c12
Andriy Arterchuk <andriyarterchuk@MacBook-Pro-Andriy.local>
2018-09-23 22:38:09 +0300

    add logic for generating coords using for loop and async iterator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 const app = express();
 const server = app.listen(3000, function () {
   console.log('listening on *:3000');
@@ -7,12 +8,74 @@ const io = require('socket.io').listen(server);
 const yargs = require('yargs');
 const argv = yargs.argv;
 
-const generateCoordinates = () => {
+const createGroupedArray = (arr, chunkSize) => {
+  const groups = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    groups.push(arr.slice(i, i + chunkSize));
+  }
+  return groups;
+}
+const generateInitialCoords = () => {
   return {
     lat: (Math.floor(Math.random() * (5200 - 4400 + 1)) + 4400) / 100,
     lng: (Math.floor(Math.random() * (4000 - 2200 + 1)) + 2200) / 100,
   }
 }
+let initialCoords = Array(argv.length || 100).fill(1).map(item => generateInitialCoords());
+
+const generateCoords = () => {
+  for (let i = 0; i < initialCoords.length; i++) {
+    const { lat, lng } = initialCoords[i];
+    initialCoords[i] = { lat: +(lat + 0.1).toFixed(2), lng };
+  }
+  return initialCoords;
+}
+
+let index = 0;
+const asyncIterator = {
+  next: () => {
+    if (index >= initialCoords.length) {
+      return Promise.resolve({ done: true });
+    }
+    const value = initialCoords[index++];
+    return Promise.resolve({ value, done: false });
+  }
+};
+
+const asyncIterable = {
+  [Symbol.asyncIterator]: () => asyncIterator
+};
+
+async function main() {
+  for await (const value of asyncIterable) {
+    const { lat, lng } = value;
+    initialCoords[index - 1] = { lat: +(lat + 0.1).toFixed(2), lng };
+  }
+  index = 0
+}
+
+async function sendByAsyncIterator(socket) {
+  await main().catch(error => console.error(error.stack));
+  socket.emit('coordinates', initialCoords);
+}
+
+// if (isMainThread) {
+//   const result = [];
+//   createGroupedArray(initialCoords, initialCoords.length / 4).map(arrayPart => {
+//     let w = new Worker(__filename, { workerData: arrayPart });
+//     w.on('message', msg => {
+//       result.push(...msg)
+//     })
+//     w.on('error', (e) => console.error('ee', e));
+//     w.on('exit', (code) => {
+//       if (code != 0)
+//         console.error(new Error(`Worker stopped with exit code ${code}`))
+//     });
+//   });
+// } else {
+//   const result = workerData.map(item => ({ lat: +(item.lat + 0.1).toFixed(2), lng: item.lng }));
+//   parentPort.postMessage(result);
+// }
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
@@ -30,7 +93,11 @@ io.on('connection', (socket) => {
   console.log('user connected');
   socket.on('fetchCoordinates', () => {
     setInterval(() => {
-      socket.emit('coordinates', Array(argv.length || 100).fill(1).map(item => generateCoordinates()));
+      // async iterator
+      sendByAsyncIterator(socket);
+
+      // for loop
+      // socket.emit('coordinates', generateCoords());
     }, 1000);
   });
 });
